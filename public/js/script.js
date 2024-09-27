aed518816730c7e4664b7b35de73e2c0ef2913e0
Jansen <jan.mangu.fernan@gmail.com>
2024-09-27 17:12:07 +0800

    minor tweaks

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
     const header = document.getElementById("scrollHeader");
-    if (window.pageYOffset > 100) {
+    if (window.pageYOffset > 70) {
         header.style.transform = "translateY(-100%)";
     } else {
         header.style.transform = "translateY(0)";
