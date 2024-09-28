a7ca70caa45b459f75109afd0c177d533415e2b9
Jansen <jan.mangu.fernan@gmail.com>
2024-09-28 23:23:07 +0800

    added more column on the service table of the data base and changed the name of the class of some elements, and currently debuging the image display for the .service class

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,7 +1,9 @@
 -- Up
 CREATE TABLE Services (
     service_name STRING,
-    service_description STRING
+    service_description STRING,
+    service_price DECIMAL(10, 2),
+    service_img BLOB
 );
 
 -- Down
--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,8 @@
 window.onscroll = function() {
-    const header = document.getElementById("scrollHeader");
-    if (window.pageYOffset > 70) {
+    const header = document.getElementById("scroll_header");
+    if (window.pageYOffset > 60) {
         header.style.transform = "translateY(-100%)";
     } else {
         header.style.transform = "translateY(0)";
     }
-};
+}
