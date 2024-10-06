9ecf81b8508749b9d4607f754d664a9020003dad
Jansen <jan.mangu.fernan@gmail.com>
2024-10-06 23:01:21 +0800

    added a pannel

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,24 @@ window.onscroll = function() {
         header.style.transform = "translateY(0)";
     }
 }
+
+function openPanel(serviceName, serviceDescription) {
+    const panel = document.getElementById('servicePanel');
+    const panelServiceName = document.getElementById('panelServiceName');
+    const panelServiceDescription = document.getElementById('panelServiceDescription');
+  
+    panelServiceName.innerHTML = serviceName;
+    panelServiceDescription.innerHTML = serviceDescription;
+
+    panel.style.display = 'block';
+}
+function closePanel() {
+    const panel = document.getElementById('servicePanel');
+    panel.style.display = 'none';
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        closePanel();
+    }
+});
